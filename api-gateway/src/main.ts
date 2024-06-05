079016d1541ddadbf8c279187c1e67732cec0c3f
Sayyad, Aasim <asayyad@ptc.com>
2024-06-05 18:50:29 +0530

    feat(add-service-routes)

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ProxyMiddleware } from './middleware/proxy.middleware';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Use body-parser middleware
+  app.use(bodyParser.json());
+
+  // Use the proxy middleware
+  app.use(new ProxyMiddleware().use);
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  console.log(`API GATEWAY STARTED on port ${PORT}`);
 }
 bootstrap();
---
+++ api-gateway/src/middleware/proxy.middleware.ts
@@ -0,0 +1,27 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const routes = {
+  '/products': process.env.PRDODUCT_URL,
+  '/customer': process.env.CUSTOMER_URL,
+  '/orders': process.env.ORDER_URL,
+  '/notification': process.env.NOTFICATION_URL,
+};
+
+@Injectable()
+export class ProxyMiddleware implements NestMiddleware {
+  use(req: any, res: any, next: () => void) {
+    const route = Object.keys(routes).find((r) => req.url.startsWith(r));
+    if (route) {
+      const target = routes[route];
+      console.log(target);
+
+      createProxyMiddleware({ target, changeOrigin: false })(req, res, next);
+    } else {
+      next();
+    }
+  }
+}
