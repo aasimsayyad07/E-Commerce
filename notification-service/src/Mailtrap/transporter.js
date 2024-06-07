89348726dce574c315c4f90e044582da3e308c6d
Sayyad, Aasim <asayyad@ptc.com>
2024-06-07 10:46:26 +0530

    refactor(add js-doc comments and linting)

--- notification-service/src/Mailtrap/transporter.js
+++ notification-service/src/Mailtrap/transporter.js
@@ -1,6 +1,9 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
+/**
+ * @description create Node Mailer Transporter along with SMTP Server Details
+ */
 const transporter = nodemailer.createTransport({
   host: process.env.MAILTRAP_HOST,
   port: process.env.MAILTRAP_PORT,
