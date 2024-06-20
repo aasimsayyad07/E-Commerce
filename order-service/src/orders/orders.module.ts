166bb997c7b504c2cbfda0a79663a6bb36eecfd3
Sayyad, Aasim <asayyad@ptc.com>
2024-06-20 17:58:36 +0530

    refactor(remove-redefination of mq modules)

--- order-service/src/orders/orders.module.ts
+++ order-service/src/orders/orders.module.ts
@@ -1,28 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Order, OrderSchema } from 'src/orders/schema/order.schema';
 import { OrdersController } from './orders.controller';
 import { OrdersProducerService } from './ordersProducer.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { OrdersConsumerService } from './ordersConsumer.service';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'ORDER-SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://localhost:5672'],
-          queue: 'order-queue',
-          queueOptions: {
-            durable: true,
-          },
-        },
-      },
-    ]),
-    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
-  ],
   providers: [OrdersProducerService, OrdersConsumerService],
   controllers: [OrdersController],
 })
